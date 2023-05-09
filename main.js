6d40177dca754cf199978ae600d1b8ec439811a6
maxi <maximo.colombatto@gmail.com>
2023-05-09 12:43:40 -0300

    termine carrusel con jquery

--- main.js
+++ main.js
@@ -31,15 +31,38 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   touch: false
 }) */
 
-
+const $botonl = $("#boton");
 const $botonr = $("#botonR");
-const imgs = $(".img-carrusel");
-console.log(imgs);
+const $imgs = $(".carrusel__item");
+const $slider = $("#slider");
+const $last = $(".carrusel__item")[$(".carrusel__item").length-1];
+$slider.prepend($last);
+const next = ()=>{
+  const $first = $(".carrusel__item")[0];
+  $imgs.addClass("slide");
+  $imgs.css("transition", "all 0.4s ease-in-out");
+  setTimeout((e)=>{
+    $imgs.css("transition", "none");
+    $imgs.removeClass("slide");
+    $slider.append($first);
+  },400);
+}
+const prev = ()=>{
+  const $last = $(".carrusel__item")[$(".carrusel__item").length-1];
+  $imgs.addClass("slidel");
+  $imgs.css("transition", "all 0.4s ease-in-out");
+  setTimeout((e)=>{
+    $imgs.css("transition", "none");
+    $imgs.removeClass("slidel");
+    $slider.prepend($last);
+  },400);
+}
 $botonr.click((e)=>{
-  const firstImg = $(".img-carrusel")[0];
-  $imgs.toggleClass("slide");
-  setTimeout(()=>{
-    $(".carrusel").insertAdjacentElement('beforeend', firstImg)
-    $imgs.toggleClass("slide");
-  },800);
+  next();
 });
+$botonl.click((e)=>{
+  prev();
+});
+setInterval((e)=>{
+  next();
+},5000);
